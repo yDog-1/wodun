9b444d247db007be6da91efcd30b0ccede519ae9
yDog <ydog-1@outlook.jp>
2025-01-26 11:26:11 +0900

    feat: testcontainers-goを使用する

---
+++ backend/pkg/testing/container/mysql.go
@@ -0,0 +1,94 @@
+package container
+
+import (
+	"context"
+	"database/sql"
+	"os"
+	"testing"
+
+	"github.com/pressly/goose/v3"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/mysql"
+)
+
+type mySQLcontainerInput struct {
+	image    string
+	dbName   string
+	user     string
+	password string
+}
+
+type MySQLcontainerOption func(*mySQLcontainerInput)
+
+func MySQLcontainerInput(options ...MySQLcontainerOption) *mySQLcontainerInput {
+	input := &mySQLcontainerInput{
+		image:    "mysql:8.0-debian",
+		dbName:   "wodun",
+		user:     "testuser",
+		password: "password",
+	}
+	for _, o := range options {
+		o(input)
+	}
+	return input
+}
+
+func WithImage(image string) MySQLcontainerOption {
+	return func(input *mySQLcontainerInput) {
+		input.image = image
+		return
+	}
+}
+
+func WithDBName(dbName string) MySQLcontainerOption {
+	return func(input *mySQLcontainerInput) {
+		input.dbName = dbName
+		return
+	}
+}
+
+func WithUser(user string) MySQLcontainerOption {
+	return func(input *mySQLcontainerInput) {
+		input.user = user
+		return
+	}
+}
+
+func WithPassword(password string) MySQLcontainerOption {
+	return func(input *mySQLcontainerInput) {
+		input.password = password
+		return
+	}
+}
+
+func MysqlContainer(t *testing.T, ctx context.Context, input *mySQLcontainerInput) (db *sql.DB, terminate func()) {
+	t.Helper()
+
+	mysqlContainer, err := mysql.Run(ctx,
+		input.image,
+		mysql.WithDatabase(input.dbName),
+		mysql.WithUsername(input.user),
+		mysql.WithPassword(input.password),
+	)
+
+	require.NoError(t, err)
+	connectionString, err := mysqlContainer.ConnectionString(ctx)
+	require.NoError(t, err)
+
+	d, err := sql.Open("mysql", connectionString)
+	require.NoError(t, err)
+	// マイグレーションファイルを取得
+	migrationsDir := "../sql/migrations"
+	p, err := goose.NewProvider(goose.DialectMySQL, d, os.DirFS(migrationsDir))
+	require.NoError(t, err)
+	// マイグレーションを実行
+	_, err = p.Up(ctx)
+	require.NoError(t, err)
+
+	return d, func() {
+		if err := testcontainers.TerminateContainer(mysqlContainer); err != nil {
+			require.NoError(t, err)
+		}
+	}
+}
--- backend/service/user_test.go
+++ backend/service/user_test.go
@@ -2,36 +2,42 @@ package service_test
 
 import (
 	"context"
-	"database/sql"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/yDog-1/wodun/backend/graph/model"
 	"github.com/yDog-1/wodun/backend/pkg"
+	"github.com/yDog-1/wodun/backend/pkg/testing/container"
 	"github.com/yDog-1/wodun/backend/repository"
 	"github.com/yDog-1/wodun/backend/service"
 )
 
-const dbURI = "testuser:password@tcp(localhost:3306)/wodun?parseTime=true"
-
 func Test_ユーザーを作成する(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
-	db, err := sql.Open("mysql", dbURI)
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	db, terminate := container.MysqlContainer(
+		t,
+		ctx,
+		container.MySQLcontainerInput(),
+	)
+	defer terminate()
+
 	repo := repository.NewUserRepository(db)
 	s := service.NewUserService(repo)
-	err = s.DeleteUser(ctx, "ydog")
-	assert.Nil(t, err)
+	err := s.DeleteUser(ctx, "ydog")
+	require.NoError(t, err)
 	input := model.CreateUserInput{
 		UniqueName:  "ydog",
 		DisplayName: "yDog",
 		Email:       "ydog@example.com",
 	}
+
 	id, err := s.CreateUser(ctx, &input)
-	assert.Nil(t, err)
+	require.NoError(t, err)
 	assert.NotEqual(t, "", id)
 
 	user, err := s.GetUser(ctx, "ydog")
@@ -43,31 +49,37 @@ func Test_ユーザーを作成する(t *testing.T) {
 }
 
 func Test_ユーザーの情報を更新する(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
-	db, err := sql.Open("mysql", dbURI)
-	if err != nil {
-		t.Fatal(err)
-	}
+
+	db, terminate := container.MysqlContainer(
+		t,
+		ctx,
+		container.MySQLcontainerInput(),
+	)
+	defer terminate()
+
 	repo := repository.NewUserRepository(db)
 	s := service.NewUserService(repo)
-	err = s.DeleteUser(ctx, "ydog")
-	assert.Nil(t, err)
+	err := s.DeleteUser(ctx, "ydog")
+	require.Nil(t, err)
 	err = s.DeleteUser(ctx, "modified")
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	input := model.CreateUserInput{
 		UniqueName:  "ydog",
 		DisplayName: "yDog",
 		Email:       "ydog@example.com",
 	}
 	id, err := s.CreateUser(ctx, &input)
-	assert.Nil(t, err)
-	assert.NotEqual(t, "", id)
+	require.Nil(t, err)
+	require.NotEqual(t, "", id)
 
 	err = s.UpdateUser(ctx, id, &model.UpdateUserInput{
 		UniqueName: pkg.PtrStr("modified"),
 		Email:      pkg.PtrStr("modified.ydog@example.com"),
 	})
-	assert.Nil(t, err)
+	require.Nil(t, err)
 
 	user, err := s.GetUser(ctx, "modified")
 	assert.Nil(t, err)
